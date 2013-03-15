4a67da1e7823c45c9fc808e7742066d02c6882bd
Daniel Somers <d.somers@ulcc.ac.uk>
2013-03-15 12:22:06 +0000

    updated caps

--- db/access.php
+++ db/access.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Jump Menu block caps.
+ * LSBU Course Overview caps.
  *
  * @package    block_lsbu_course_overview
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
