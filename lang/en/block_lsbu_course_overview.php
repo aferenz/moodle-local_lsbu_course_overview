c2daf137618e668edd2f5188b6490f3ed2c44e60
Daniel Somers <d.somers@ulcc.ac.uk>
2013-03-21 11:19:28 +0000

    added caps

--- lang/en/block_lsbu_course_overview.php
+++ lang/en/block_lsbu_course_overview.php
@@ -1,4 +1,7 @@
 <?php
 $string['pluginname'] = 'LSBU Course overview';
 
+$string['lsbu_course_overview:addinstance'] = 'Add a new logged in user block';
+$string['lsbu_course_overview:myaddinstance'] = 'Add a new logged in user block to the My Moodle page';
+
 
