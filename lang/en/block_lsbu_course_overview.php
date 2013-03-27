28c7a46060c3ebc942013b659ac6748ca06023bc
ian.wild <ian.wild@heavy-horse.co.uk>
2013-03-27 08:18:42 +0000

    New language strings added for settings page.

--- lang/en/block_lsbu_course_overview.php
+++ lang/en/block_lsbu_course_overview.php
@@ -4,4 +4,22 @@ $string['pluginname'] = 'LSBU Course overview';
 $string['lsbu_course_overview:addinstance'] = 'Add a new logged in user block';
 $string['lsbu_course_overview:myaddinstance'] = 'Add a new logged in user block to the My Moodle page';
 
+// Months of the year
+$string['january'] = 'January';
+$string['february'] = 'February';
+$string['march'] = 'March';
+$string['april'] = 'April';
+$string['may'] = 'May';
+$string['june'] = 'June';
+$string['july'] = 'July';
+$string['august'] = 'August';
+$string['september'] = 'September';
+$string['october'] = 'October';
+$string['november'] = 'November';
+$string['december'] = 'December';
+
+$string['academicyearstart'] = 'Start of academic year';
+$string['configacademicyearstart'] = 'Select the month corresponding to the start of the academic year. This setting dictates how future, current and previous year\'s courses are grouped for display.';
+
+
 
