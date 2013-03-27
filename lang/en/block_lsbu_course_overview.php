2c2338df83ff61ecd9955ca483fe43eba616b7c3
ian.wild <ian.wild@heavy-horse.co.uk>
2013-03-27 10:10:06 +0000

    Added language strings for new configuration settings.

--- lang/en/block_lsbu_course_overview.php
+++ lang/en/block_lsbu_course_overview.php
@@ -4,6 +4,8 @@ $string['pluginname'] = 'LSBU Course overview';
 $string['lsbu_course_overview:addinstance'] = 'Add a new logged in user block';
 $string['lsbu_course_overview:myaddinstance'] = 'Add a new logged in user block to the My Moodle page';
 
+// Admin Settings page
+
 // Months of the year
 $string['january'] = 'January';
 $string['february'] = 'February';
@@ -21,5 +23,47 @@ $string['december'] = 'December';
 $string['academicyearstart'] = 'Start of academic year';
 $string['configacademicyearstart'] = 'Select the month corresponding to the start of the academic year. This setting dictates how future, current and previous year\'s courses are grouped for display.';
 
+// Identifying the academic year
+$string['academicyearheader'] = 'Academic year';
 
+$string['academicyearfield'] = 'Academic year field';
+$string['configacademicyearfield'] = 'Which field (e.g. \'idnumber\' or \'shortname\') contains the academic year code?';
+$string['academicyearfielddefault'] = 'idnumber';
 
+$string['academicyearregexp'] = 'Academic year pattern (regular expression)';
+$string['configacademicyearregexp'] = 'A regular expression used to match the academic year. See \'http://en.wikipedia.org/wiki/Regular_expression\' for details';
+$string['academicyearregexpdefault'] = '/[0-9]{4}$/';
+
+$string['courseidentificationheader'] = 'Identifying a \'course\'';
+
+$string['coursefield'] = 'Course field';
+$string['configcoursefield'] = 'Which field (e.g. \'idnumber\' or \'shortname\') contains course type code (indicating a Moodle course is a \'course\')?';
+$string['coursefielddefault'] = 'idnumber';
+
+$string['courseregexp'] = 'Academic year pattern (regular expression)';
+$string['configcourseregexp'] = 'A regular expression used to match the course code. See \'http://en.wikipedia.org/wiki/Regular_expression\' for details';
+$string['courseregexpdefault'] = '/^CRS_/';
+
+$string['moduleidentificationheader'] = 'Identifying a \'module\'';
+
+$string['modulefield'] = 'Module field';
+$string['configmodulefield'] = 'Which field (e.g. \'idnumber\' or \'shortname\') course type code that indicates a Moodle course is a \'module\'?';
+$string['modulefielddefault'] = 'idnumber';
+
+$string['moduleregexp'] = 'Academic year pattern (regular expression)';
+$string['configmoduleregexp'] = 'A regular expression used to match the module code. See \'http://en.wikipedia.org/wiki/Regular_expression\' for details';
+$string['moduleregexpdefault'] = '/^MOD_/';
+
+$string['supportcategoryheader'] = 'Support category';
+
+$string['supportcategory'] = 'Support category';
+$string['configsupportcategory'] = 'Category containing support courses. The comparison is case insensitive.';
+$string['supportcategorydefault'] = 'support';
+
+$string['studentsupportcategoryheader'] = 'Student support category';
+
+$string['studentsupportcategory'] = 'Student support category';
+$string['configstudentsupportcategory'] = 'Category containing student support courses. The comparison is case insensitive.';
+$string['studentsupportcategorydefault'] = 'student support';
+
+// 
