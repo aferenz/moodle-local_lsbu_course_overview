35c0308770e3148b1bdbade1afc1a86af01d98f4
ian.wild <ian.wild@heavy-horse.co.uk>
2013-03-27 10:09:25 +0000

    Improvement: there are lots of options for identifying courses, modules, student support courses, etc. Make these admin settings.

--- settings.php
+++ settings.php
@@ -3,6 +3,11 @@
 defined('MOODLE_INTERNAL') || die;
 
 if ($ADMIN->fulltree) {
+
+	
+	// Start of academic year
+	$settings->add(new admin_setting_heading('block_lsbu_course_overview_academicyearheader', get_string('academicyearheader', 'block_lsbu_course_overview'), ''));
+			
     $options = array('1'=>get_string('january', 'block_lsbu_course_overview'),
     				'2'=>get_string('february', 'block_lsbu_course_overview'),
     				'3'=>get_string('march', 'block_lsbu_course_overview'),
@@ -18,4 +23,39 @@ if ($ADMIN->fulltree) {
 
     $settings->add(new admin_setting_configselect('block_lsbu_course_overview/academicyearstart', get_string('academicyearstart', 'block_lsbu_course_overview'),
                        get_string('configacademicyearstart', 'block_lsbu_course_overview'), 'all', $options));
+    
+    // Field and pattern matching for academic year
+    $settings->add(new admin_setting_configtext('block_lsbu_course_overview/academicyearfield', new lang_string('academicyearfield', 'block_lsbu_course_overview'),
+    					get_string('configacademicyearfield', 'block_lsbu_course_overview'), get_string('academicyearfielddefault', 'block_lsbu_course_overview'), PARAM_TEXT));
+    $settings->add(new admin_setting_configtext('block_lsbu_course_overview/academicyearregexp', new lang_string('academicyearregexp', 'block_lsbu_course_overview'),
+			    		get_string('configacademicyearregexp', 'block_lsbu_course_overview'), get_string('academicyearregexpdefault', 'block_lsbu_course_overview'), PARAM_TEXT));
+    
+    // Field and pattern matching for courses
+    $settings->add(new admin_setting_heading('block_lsbu_course_overview_courseidentificationheader', get_string('courseidentificationheader', 'block_lsbu_course_overview'), ''));
+    
+    $settings->add(new admin_setting_configtext('block_lsbu_course_overview/coursefield', new lang_string('coursefield', 'block_lsbu_course_overview'),
+    					get_string('configcoursefield', 'block_lsbu_course_overview'), get_string('coursefielddefault', 'block_lsbu_course_overview'), PARAM_TEXT));
+    $settings->add(new admin_setting_configtext('block_lsbu_course_overview/courseregexp', new lang_string('courseregexp', 'block_lsbu_course_overview'),
+			    		get_string('configcourseregexp', 'block_lsbu_course_overview'), get_string('courseregexpdefault', 'block_lsbu_course_overview'), PARAM_TEXT));
+     
+    // Field and pattern matching for modules
+    $settings->add(new admin_setting_heading('block_lsbu_course_overview_moduleidentificationheader', get_string('moduleidentificationheader', 'block_lsbu_course_overview'), ''));
+    
+    $settings->add(new admin_setting_configtext('block_lsbu_course_overview/modulefield', new lang_string('modulefield', 'block_lsbu_course_overview'),
+    					get_string('configmodulefield', 'block_lsbu_course_overview'), get_string('modulefielddefault', 'block_lsbu_course_overview'), PARAM_TEXT));
+    $settings->add(new admin_setting_configtext('block_lsbu_course_overview/moduleregexp', new lang_string('moduleregexp', 'block_lsbu_course_overview'),
+    					get_string('configmoduleregexp', 'block_lsbu_course_overview'), get_string('moduleregexpdefault', 'block_lsbu_course_overview'), PARAM_TEXT));
+    
+    // Category for support
+    $settings->add(new admin_setting_heading('block_lsbu_course_overview_supportcategoryheader', get_string('supportcategoryheader', 'block_lsbu_course_overview'), ''));
+    
+    $settings->add(new admin_setting_configtext('block_lsbu_course_overview/supportcategory', new lang_string('supportcategory', 'block_lsbu_course_overview'),
+    		get_string('configsupportcategory', 'block_lsbu_course_overview'), get_string('supportcategorydefault', 'block_lsbu_course_overview'), PARAM_TEXT));
+    
+    // Category for student support
+    $settings->add(new admin_setting_heading('block_lsbu_course_overview_studentsupportcategoryheader', get_string('studentsupportcategoryheader', 'block_lsbu_course_overview'), ''));
+    
+    $settings->add(new admin_setting_configtext('block_lsbu_course_overview/studentsupportcategory', new lang_string('studentsupportcategory', 'block_lsbu_course_overview'),
+    		get_string('configstudentsupportcategory', 'block_lsbu_course_overview'), get_string('studentsupportcategorydefault', 'block_lsbu_course_overview'), PARAM_TEXT));
+    
 }
